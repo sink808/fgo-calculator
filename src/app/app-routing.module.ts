e68b3790ae2633e0e2dbe24a42f60e7488847cd8
Heimer <heimer@25sprout.com>
2020-07-09 14:20:46 +0800

    feat: set main component route to root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { MainComponent } from './main/main.component';
+
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'main' },
-  { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) }
+  // { path: '', pathMatch: 'full', redirectTo: 'main' },
+  { path: '', loadChildren: () => import('./main/main.module').then(m => m.MainModule) }
 ];
 
 @NgModule({
