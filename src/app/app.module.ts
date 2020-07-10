444fb3157df194862ff63bf1b3c64487d56c60cc
Heimer <heimer@25sprout.com>
2020-07-10 17:42:40 +0800

    feat: takaragu html prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/shareModule/mat.module.ts
+++ src/app/shareModule/mat.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @NgModule({
   imports: [
-    MatTabsModule
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCheckboxModule
   ],
   exports: [
-    MatTabsModule
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCheckboxModule
   ]
 })
 export class MatModule { }
