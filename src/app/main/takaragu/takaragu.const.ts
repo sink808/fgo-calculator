4af135fa50def2d296b1f4f743aaf2453034f1b8
Heimer <heimer@25sprout.com>
2020-07-16 12:32:03 +0800

    feat: modify some values of takaragu const

--- src/app/main/takaragu/takaragu.const.ts
+++ src/app/main/takaragu/takaragu.const.ts
@@ -13,7 +13,10 @@ export const NP_BUFF = 'npBuff';
 export const SPECIAL_BUFF = 'specialBuff';
 export const SPECIAL_NP_BUFF = 'specialNpBuff';
 export const FIXED_BUFF = 'fixedBuff';
-export const CORRECTION = 'Correction';
+
+export const MAX_DAMAGE = 'maxDamage';
+export const MIN_DAMAGE = 'minDamage';
+export const AVG_DAMAGE = 'avgDamage';
 
 export interface TakaraguModels {
   [ATK]: number;
@@ -29,6 +32,9 @@ export interface TakaraguModels {
   [SPECIAL_BUFF]: number;
   [SPECIAL_NP_BUFF]: number;
   [FIXED_BUFF]: number;
+  [MAX_DAMAGE]?: number;
+  [MIN_DAMAGE]?: number;
+  [AVG_DAMAGE]?: number;
 }
 
 const classSelectOptions: SelectOption[] = [
@@ -45,6 +51,7 @@ const classSelectOptions: SelectOption[] = [
   {title: 'Foreigner ', value: 1},
   {title: 'Alterego ', value: 1},
   {title: 'MoonCancer', value: 1},
+  {title: 'Beast', value: 1}
 ];
 
 const classInhibitionSelectOptions: SelectOption[] = [
@@ -73,17 +80,16 @@ export const mainFormItems: FormFieldItem[] = [
   {title: '職階克制', type: 'select', modelName: CLASS_INHIBITION, selectOptions: classInhibitionSelectOptions, initialValue: 2},
   {title: '陣營克制', type: 'select', modelName: GROUP_INHIBITION, selectOptions: groupInhibitionSelectOptions, initialValue: 1},
   {title: '寶具顏色', type: 'select', modelName: NP_CARD, selectOptions: cardSelectOptions, initialValue: 1.5},
-  {title: '寶具倍率', type: 'input', modelName: NP_POWER, initialValue: 0}
+  {title: '寶具倍率(%)', type: 'input', modelName: NP_POWER, initialValue: 0}
 ];
 
 export const subFormItems: FormFieldItem[] = [
   {title: '禮裝ATK', type: 'input', modelName: EQUIPMENT_ATK, initialValue: 0},
-  {title: 'ATK Buff(%)', type: 'input', modelName: ATK_BUFF, initialValue: 100, tooltip: '攻擊力Buff — 敵方防禦力Buff — 敵方特防威力Buff'},
-  {title: '色卡 Buff(%)', type: 'input', modelName: CARD_BUFF, initialValue: 100, tooltip: '色卡Buff — 敵方色卡耐性Buff'},
-  {title: '寶具威力 Buff(%)', type: 'input', modelName: NP_BUFF, initialValue: 100},
-  {title: '特攻威力 Buff(%)', type: 'input', modelName: SPECIAL_BUFF, initialValue: 100},
+  {title: 'ATK Buff(%)', type: 'input', modelName: ATK_BUFF, initialValue: 0, tooltip: '攻擊力Buff — 敵方防禦力Buff — 敵方特防威力Buff'},
+  {title: '色卡 Buff(%)', type: 'input', modelName: CARD_BUFF, initialValue: 0, tooltip: '色卡Buff — 敵方色卡耐性Buff'},
+  {title: '寶具威力 Buff(%)', type: 'input', modelName: NP_BUFF, initialValue: 0},
+  {title: '特攻威力 Buff(%)', type: 'input', modelName: SPECIAL_BUFF, initialValue: 0},
   {title: '寶具特攻 Buff(%)', type: 'input', modelName: SPECIAL_NP_BUFF, initialValue: 100},
-  {title: '攻擊補正 (%)', type: 'input', modelName: CORRECTION, initialValue: 100},
   {title: '固定傷害 Buff', type: 'input', modelName: FIXED_BUFF, initialValue: 0}
 ];
 
