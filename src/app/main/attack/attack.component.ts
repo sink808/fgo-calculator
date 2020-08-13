cb95dc2ec7d091d80a355b5a2d72223d7543e3de
Heimer <heimer@25sprout.com>
2020-08-13 10:36:19 +0800

    feat: add attack component and setting file

--- src/app/main/attack/attack.component.ts
+++ src/app/main/attack/attack.component.ts
@@ -1,17 +1,135 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { MainService } from '../main.service';
+import { FormFieldItem } from '../component/form-fields/form-fields.const';
+import { ColModel } from '../component/table/table.const';
+import {
+  CARD,
+  CARD_ORDER,
+  FIRST_CARD,
+  BUSTER_CHAIN,
+  IS_CRITICAL,
+  CRITICAL_BUFF,
+  AtkModels,
+  AtkColModels,
+  mainFormItems,
+  subFormItems,
+  displayedColumns,
+  cardDmgSelectOptions,
+  cardOrderSelectOptions,
+  firstCardSelectOptions,
+  busterChainSelectOptions,
+  isCriticalSelectOptions } from './attack.const';
+import {
+  ATK,
+  CLASS,
+  CLASS_INHIBITION,
+  GROUP_INHIBITION,
+  EQUIPMENT_ATK,
+  ATK_BUFF,
+  CARD_BUFF,
+  SPECIAL_BUFF,
+  FIXED_BUFF,
+  MAX_DAMAGE,
+  MIN_DAMAGE,
+  AVG_DAMAGE,
+  classSelectOptions,
+  classInhibitionSelectOptions,
+  groupInhibitionSelectOptions } from '../main.const';
 @Component({
   selector: 'app-attack',
   templateUrl: './attack.component.html',
   styleUrls: ['./attack.component.scss']
 })
 export class AttackComponent implements OnInit {
-
-  constructor(private title: Title) {
+  public mainFormItems: FormFieldItem[] = mainFormItems;
+  public subFormItems: FormFieldItem[] = subFormItems;
+  public damageList: AtkColModels[] = []; // all cols value for table
+  public colModels: ColModel[] = this.getColModels(); // table cols title and key
+  public displayedColumns: string[] = displayedColumns; // table main cols key
+  constructor(
+    private title: Title,
+    private mainService: MainService
+  ) {
     this.title.setTitle('指令卡傷害計算');
   }
 
   ngOnInit(): void {
   }
 
+  private getColModels(): ColModel[] {
+    const model = [];
+    this.mainFormItems.forEach((item) => model.push({title: item.title, key: item.modelName}));
+    this.subFormItems.forEach((item) => model.push({title: item.title, key: item.modelName}));
+    model.push(
+      { title: '最大傷害', key: MAX_DAMAGE },
+      { title: '最小傷害', key: MIN_DAMAGE },
+      { title: '平均傷害', key: AVG_DAMAGE }
+    );
+    return model;
+  }
+
+  public calculate(value: AtkModels): void {
+    console.log(value);
+    /* 公式 =
+    ATK ×
+    攻擊補正 ×
+    [ 卡牌傷害倍率 × 位置加成 × ( 1 + 卡牌BUFF ) + 首位加成 ] ×
+    職階補正 ×
+    職階相性補正 ×
+    陣營相性補正 ×
+    亂數補正 ×
+    ( 1 + 攻擊力BUFF — 敵方防禦力BUFF — 敵方特防威力BUFF ) ×
+    ( 1 + 特攻威力BUFF + 暴擊威力BUFF ) ×
+    暴擊補正 ×
+    EX攻擊獎勵 +
+    ( 固定傷害BUFF — 敵方固定傷害BUFF ) +
+    ATK × Buster Chain加成
+    */
+
+    const atk: number = (value[ATK] + value[EQUIPMENT_ATK]);
+    const classValue: number = value[CLASS];
+    const classInhibition: number = value[CLASS_INHIBITION];
+    const groupInhibition: number = value[GROUP_INHIBITION];
+    const atkBuff: number = (1 + (value[ATK_BUFF] / 100));
+    const card: number = (value[CARD] === 2 || value[CARD] === 3.5) ? 1 : value[CARD];
+    const cardBuff: number = card * value[CARD_ORDER] * (1 + value[CARD_BUFF] / 100) + value[FIRST_CARD];
+    const critical: number = value[IS_CRITICAL];
+    const criticalBuff: number = (value[IS_CRITICAL] === 1) ? 0 : (value[CRITICAL_BUFF] / 100);
+    const specialBuff: number = (1 + (value[SPECIAL_BUFF] / 100) + criticalBuff);
+    const exBuff: number = (value[CARD] !== 2 && value[CARD] !== 3.5) ? 1 : value[CARD];
+    const correction = 0.23;
+    const maxRandomNum = 1.1;
+    const minRandomNum = 0.9;
+    const fixedValue: number = value[FIXED_BUFF];
+    const busterChain: number = atk * value[BUSTER_CHAIN];
+    const normalValue: number = atk * correction * cardBuff * classValue * classInhibition *
+      groupInhibition * atkBuff * specialBuff * critical * exBuff;
+    const col: AtkColModels = modelsToColModels.call(this);
+
+    this.damageList = [col, ...this.damageList]; // for @Input change
+
+    function modelsToColModels(): AtkColModels {
+      return {
+        [ATK]: value[ATK],
+        [CLASS]: this.mainService.getOptionTitle(value[CLASS], classSelectOptions),
+        [CLASS_INHIBITION]: this.mainService.getOptionTitle(value[CLASS_INHIBITION], classInhibitionSelectOptions),
+        [GROUP_INHIBITION]: this.mainService.getOptionTitle(value[GROUP_INHIBITION], groupInhibitionSelectOptions),
+        [CARD]: this.mainService.getOptionTitle(value[CARD], cardDmgSelectOptions),
+        [CARD_ORDER]: this.mainService.getOptionTitle(value[CARD_ORDER], cardOrderSelectOptions),
+        [FIRST_CARD]: this.mainService.getOptionTitle(value[FIRST_CARD], firstCardSelectOptions),
+        [BUSTER_CHAIN]: this.mainService.getOptionTitle(value[BUSTER_CHAIN], busterChainSelectOptions),
+        [EQUIPMENT_ATK]: value[EQUIPMENT_ATK],
+        [ATK_BUFF]: value[ATK_BUFF],
+        [CARD_BUFF]: value[CARD_BUFF],
+        [SPECIAL_BUFF]: value[SPECIAL_BUFF],
+        [IS_CRITICAL]: this.mainService.getOptionTitle(value[IS_CRITICAL], isCriticalSelectOptions),
+        [CRITICAL_BUFF]: value[CRITICAL_BUFF],
+        [FIXED_BUFF]: value[FIXED_BUFF],
+        [MAX_DAMAGE]: normalValue * maxRandomNum + fixedValue + busterChain,
+        [MIN_DAMAGE]: normalValue * minRandomNum + fixedValue + busterChain,
+        [AVG_DAMAGE]: normalValue + fixedValue + busterChain
+      };
+    }
+  }
 }
---
+++ src/app/main/attack/attack.const.ts
@@ -0,0 +1,118 @@
+import { SelectOption, FormFieldItem } from '../component/form-fields/form-fields.const';
+import {
+  ATK,
+  CLASS,
+  CLASS_INHIBITION,
+  GROUP_INHIBITION,
+  EQUIPMENT_ATK,
+  ATK_BUFF,
+  CARD_BUFF,
+  SPECIAL_BUFF,
+  FIXED_BUFF,
+  MAX_DAMAGE,
+  MIN_DAMAGE,
+  AVG_DAMAGE,
+  classSelectOptions,
+  classInhibitionSelectOptions,
+  groupInhibitionSelectOptions,
+  cardSelectOptions
+} from '../main.const';
+
+export const CARD = 'card';
+export const CARD_ORDER = 'cardOrder';
+export const FIRST_CARD = 'firstCard';
+export const BUSTER_CHAIN = 'busterChain';
+export const IS_CRITICAL = 'isCritical';
+export const CRITICAL_BUFF = 'criticalBuff';
+
+export interface AtkModels {
+  [ATK]: number;
+  [CLASS]: number;
+  [CLASS_INHIBITION]: number;
+  [GROUP_INHIBITION]: number;
+  [CARD]: number;
+  [CARD_ORDER]: number;
+  [FIRST_CARD]: number;
+  [BUSTER_CHAIN]: number;
+  [EQUIPMENT_ATK]: number;
+  [ATK_BUFF]: number;
+  [CARD_BUFF]: number;
+  [SPECIAL_BUFF]: number;
+  [IS_CRITICAL]: number;
+  [CRITICAL_BUFF]: number;
+  [FIXED_BUFF]: number;
+}
+
+export interface AtkColModels {
+  [ATK]: number;
+  [CLASS]: string;
+  [CLASS_INHIBITION]: string;
+  [GROUP_INHIBITION]: string;
+  [CARD]: string;
+  [CARD_ORDER]: string;
+  [FIRST_CARD]: string;
+  [BUSTER_CHAIN]: string;
+  [EQUIPMENT_ATK]: number;
+  [ATK_BUFF]: number;
+  [CARD_BUFF]: number;
+  [SPECIAL_BUFF]: number;
+  [IS_CRITICAL]: string;
+  [CRITICAL_BUFF]: number;
+  [FIXED_BUFF]: number;
+  [MAX_DAMAGE]: number;
+  [MIN_DAMAGE]: number;
+  [AVG_DAMAGE]: number;
+}
+
+export const cardDmgSelectOptions: SelectOption[] = [
+  {title: 'Buster(紅)', value: 1.5},
+  {title: 'Arts(藍)', value: 1},
+  {title: 'Quick(綠)', value: 0.8},
+  {title: 'Extra Attack(前三張同色)', value: 3.5},
+  {title: 'Extra Attack(前三張不同色)', value: 2}
+];
+
+export const cardOrderSelectOptions: SelectOption[] = [
+  {title: '第1張', value: 1},
+  {title: '第2張', value: 1.2},
+  {title: '第3張', value: 1.4},
+  {title: '第4張(Extra Attack)', value: 1},
+];
+
+export const firstCardSelectOptions: SelectOption[] = [
+  {title: 'Buster(紅)', value: 0.5},
+  {title: '非Buster(紅)', value: 0},
+];
+
+export const busterChainSelectOptions: SelectOption[] = [
+  {title: '是', value: 0.2},
+  {title: '否', value: 0},
+];
+
+export const isCriticalSelectOptions: SelectOption[] = [
+  {title: '是', value: 2},
+  {title: '否', value: 1},
+];
+
+export const mainFormItems: FormFieldItem[] = [
+  {title: 'ATK', type: 'input', modelName: ATK, initialValue: 0},
+  {title: '職階', type: 'select', modelName: CLASS, selectOptions: classSelectOptions, initialValue: 1},
+  {title: '職階克制', type: 'select', modelName: CLASS_INHIBITION, selectOptions: classInhibitionSelectOptions, initialValue: 2},
+  {title: '陣營克制', type: 'select', modelName: GROUP_INHIBITION, selectOptions: groupInhibitionSelectOptions, initialValue: 1},
+  {title: '卡片顏色', type: 'select', modelName: CARD, selectOptions: cardDmgSelectOptions, initialValue: 1.5},
+  {title: '卡片位置', type: 'select', modelName: CARD_ORDER, selectOptions: cardOrderSelectOptions, initialValue: 1},
+  {title: '首卡顏色', type: 'select', modelName: FIRST_CARD, selectOptions: firstCardSelectOptions, initialValue: 0.5},
+  {title: 'Buster Chain加成', type: 'select', modelName: BUSTER_CHAIN, selectOptions: busterChainSelectOptions, initialValue: 0}
+];
+
+export const subFormItems: FormFieldItem[] = [
+  {title: '禮裝ATK', type: 'input', modelName: EQUIPMENT_ATK, initialValue: 0},
+  {title: 'ATK Buff(%)', type: 'input', modelName: ATK_BUFF, initialValue: 0, tooltip: '攻擊力Buff — 敵方防禦力Buff — 敵方特防威力Buff'},
+  {title: '色卡 Buff(%)', type: 'input', modelName: CARD_BUFF, initialValue: 0, tooltip: '色卡Buff — 敵方色卡耐性Buff'},
+  {title: '特攻威力 Buff(%)', type: 'input', modelName: SPECIAL_BUFF, initialValue: 0},
+  {title: '固定傷害 Buff', type: 'input', modelName: FIXED_BUFF, initialValue: 0},
+  {title: '是否爆擊', type: 'select', modelName: IS_CRITICAL, initialValue: 1, selectOptions: isCriticalSelectOptions},
+  {title: '爆擊威力 Buff(%)', type: 'input', modelName: CRITICAL_BUFF, initialValue: 0}
+];
+
+export const displayedColumns: string[] = [MAX_DAMAGE, MIN_DAMAGE, AVG_DAMAGE];
